d8c769d76a0d1622d0b14695768299ca2cb64697
Hack Reactor Students <ethoreby@gmail.com>
2014-02-26 12:36:39 -0800

    passing specs for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var index = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[index++] = value;
+    size++;
   };
 
   instance.dequeue = function(){
+    if(size > 0) {
+      var earliestIndex = Object.keys(storage).sort()[0];
+      var result = storage[earliestIndex];
+      delete storage[earliestIndex];
+      size--;
+      return result;
+    }
+
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
