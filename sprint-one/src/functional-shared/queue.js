17e78fabc1c5be2101654e0512e11c0e496076cb
Hack Reactor Students <ethoreby@gmail.com>
2014-02-26 15:24:07 -0800

    functional shared queues passing specs

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,30 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {
+    storage: {},
+    length: 0,
+    index: 0
+  };
+  instance = _.extend(instance, queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.index++] = value;
+    this.length++;
+  },
+
+  dequeue: function(){
+    if(this.length > 0) {
+      var earliestIndex = Object.keys(this.storage).sort()[0];
+      var result = this.storage[earliestIndex];
+      delete this.storage[earliestIndex];
+      this.length--;
+      return result;
+    }
+  },
+
+  size: function(){
+    return this.length;
+  }
+};
