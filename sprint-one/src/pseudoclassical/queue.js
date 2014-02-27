90dd53128c9f569eefcc56fb5857355ebd638628
Hack Reactor Students <ethoreby@gmail.com>
2014-02-26 16:39:50 -0800

    passing pseudoclassical stacks and queues

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,26 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(length, storage) {
+  this.storage = storage || {};
+  this.length = length || 0;
+  this.index = length || 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.index++] = value;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function() {
+  if(this.length > 0) {
+    var earliestIndex = Object.keys(this.storage).sort()[0];
+    var result = this.storage[earliestIndex];
+    delete this.storage[earliestIndex];
+    this.length--;
+    return result;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.length;
+};
+
+var queue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,23 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(length,storage) {
+  this.length = length || 0;
+  this.storage = storage || {};
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[++this.length] = value;
+};
+
+Stack.prototype.pop = function() {
+  if(this.storage[this.length] !== undefined) {
+    var result = this.storage[this.length];
+    delete this.storage[this.length];
+    this.length--;
+    return result;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
+
+var stack = new Stack();
