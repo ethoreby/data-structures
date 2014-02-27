ecf5a1c45957df0f151591deb2561a78881afc58
Hack Reactor Students <ethoreby@gmail.com>
2014-02-26 16:12:00 -0800

    passing prototypal stacks and queues

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,28 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(length, storage) {
+  var instance = Object.create(queueMethods);
+  instance.storage = storage || {};
+  instance.length = length || 0;
+  instance.index = length || 0;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+    enqueue: function(value){
+    this.storage[this.index++] = value;
+    this.length++;
+  },
+
+  dequeue: function(){
+    if(this.length > 0) {
+      var earliestIndex = Object.keys(this.storage).sort()[0];
+      var result = this.storage[earliestIndex];
+      delete this.storage[earliestIndex];
+      this.length--;
+      return result;
+    }
+  },
+
+  size: function(){
+    return this.length;
+  }
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,33 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+// var makeStack = function() {
+//   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+// };
+
+// var stackMethods = {};
+
+var makeStack = function(length, storage) {
+  var instance = Object.create(stackMethods);
+  instance.length = length || 0;
+  instance.storage = storage || {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[++this.length] = value;
+  },
+
+  pop: function(){
+    if(this.storage[this.length] !== undefined) {
+      var result = this.storage[this.length];
+      delete this.storage[this.length];
+      this.length--;
+      return result;
+    }
+  },
+
+  size: function(){
+    return this.length;
+  }
+};
+
+makeStack();
