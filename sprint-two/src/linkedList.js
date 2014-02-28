afe135733c82132b7d4fa20604746584e83c42d9
Hack Reactor Students <ethoreby@gmail.com>
2014-02-27 17:13:21 -0800

    passing linked list specs

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if(list.tail) {
+      list.tail.next = node;
+    }
+    list.tail = node;
+
+    list.head = list.head || node;
   };
 
   list.removeHead = function(){
+    if(list.head) {
+      var result = list.head.value;
+      list.head = list.head.next;
+      return result;
+    }
   };
 
   list.contains = function(target, node){
+    node = node || list.head;
+    if(node.value === target) {
+      return true;
+    } else if (node.next === null) {
+      return false;
+    }
+
+    return list.contains(target, node.next);
   };
 
   return list;
