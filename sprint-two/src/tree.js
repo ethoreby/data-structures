1d454db9334273963213390562077617d6422942
Hack Reactor Students <ethoreby@gmail.com>
2014-02-27 21:12:43 -0800

    passing specs for sets and trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,10 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  for (var key in treeMethods) {
+    newTree[key] = treeMethods[key];
+  }
   return newTree;
 };
 
@@ -9,8 +12,24 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var childTree = makeTree(value);
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target){
-};
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length === 0) {
+    return false;
+  }
 
+  var result = false;
+  for(var i = 0; i < this.children.length; i++) {
+    if(this.children[i].contains(target)) {
+      result = true;
+      break;
+    }
+  }
+
+  return result;
+};
