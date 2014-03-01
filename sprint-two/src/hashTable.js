28d95d1d8a117740fd3817f34dbaff8b804a17be
Hack Reactor Students <ethoreby@gmail.com>
2014-02-28 16:39:54 -0800

    hashtable handles collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,26 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = v;
+  if (!this._storage[i]) {
+    this._storage[i] = [];
+  }
+  this._storage[i].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i];
+  var bucket = this._storage[i];
+  if (!bucket) {
+    return null;
+  }
+  var current;
+  for (var j = 0; j < bucket.length; j++) {
+    current = bucket[j];
+    if (current[0] === k) {
+      return current[1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
